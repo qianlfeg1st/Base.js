c52b195c88b7d1fb1f14f8d9b6eb34a4c1f462dd
qianlifeng <qianlifeng@pipi.cn>
2017-04-10 17:24:13 +0800

    增加构造函数和工厂方法

--- Base.js
+++ Base.js
@@ -11,10 +11,34 @@ $ = function( selector, context ) {
   return base.init( selector, context );
 };
 
+// Base构造函数
+function B( dom, selector ) {
+  // DOM集合存在的话，就获取它的长度，不存在的话，就等于0
+  var len = dom ? dom.length : 0;
+  // 将每个DOM节点分别写入对象
+  for ( var i = 0; i < len; i++ ) {
+    this[ i ] = dom[ i ];
+  }
+  // DOM集合的长度
+  this.length = len;
+  // selector 的默认值为 空字符串
+  this.selector = selector || '';
+}
+
 base = {
   // 初始化方法
   init: function( selector, context ) {
     console.info('初始化开始!');
+    // DOM集合
+    var dom;
+
+    // selector 为未参，返回 空的Base对象
+    if ( !selector ) return this.B();
+  },
+  // 工厂方法
+  B: function( dom, selector ) {
+    // 返回 Base构造的实例
+    return new B( dom, selector );
   }
 };
 
